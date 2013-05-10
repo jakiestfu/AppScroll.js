5c359bf13e039e1c17ee5e8b78db91800008c8b2
Jacob Kelley <jakie8@gmail.com>
2013-05-09 18:44:53 -0600

    Update AppScroll.js

--- AppScroll.js
+++ AppScroll.js
@@ -1,35 +1,35 @@
 var AppScroll = AppScroll || (function(win, doc){
-  
-	var cache = {},
-		events = {
-			touchMove: function(e){
-				e.preventDefault();
-			},
-			touchEnd: function(e){
-				cache.listenForScroll = true;
-			},
-			endScroll: function(e){
-				if(cache.listenForScroll){
-					if(cache.scroller.scrollTop === 0){
-						cache.scroller.scrollTop = 1;
-						cache.listenForScroll = false;
-					}
-				} else {
-					cache.listenForScroll = false;
-				}
-			}
-		},
-		init = function(elements){
-			cache = elements;
-			
-			cache.scroller.addEventListener('touchend', events.touchEnd);
-			cache.toolbar.addEventListener('touchmove', events.touchMove);
-			cache.scroller.addEventListener('scroll', events.endScroll);
-			
-			events.touchEnd();
-			events.endScroll();
-		};
-	
-	return init;
-	
+    
+    var cache = {},
+        events = {
+            touchMove: function(e){
+                e.preventDefault();
+            },
+            touchEnd: function(e){
+                cache.listenForScroll = true;
+            },
+            endScroll: function(e){
+                if(cache.listenForScroll){
+                    if(cache.scroller.scrollTop === 0){
+                        cache.scroller.scrollTop = 1;
+                        cache.listenForScroll = false;
+                    }
+                } else {
+                    cache.listenForScroll = false;
+                }
+            }
+        },
+        init = function(elements){
+            cache = elements;
+            
+            cache.scroller.addEventListener('touchend', events.touchEnd);
+            cache.toolbar.addEventListener('touchmove', events.touchMove);
+            cache.scroller.addEventListener('scroll', events.endScroll);
+            
+            events.touchEnd();
+            events.endScroll();
+        };
+    
+    return init;
+    
 })(window, document);
